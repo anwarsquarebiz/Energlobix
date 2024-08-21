54a0b1d63dd06359af38d3a80a2ac7138df22f0a
Anwar Shaikh <anwar.squarebiz@gmail.com>
2024-08-21 16:45:13 +0530

    up arrow added

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,3 +92,23 @@ function toggleSubmenu(id) {
         submenu.classList.add("show");
     }
 }
+
+
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+backToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
